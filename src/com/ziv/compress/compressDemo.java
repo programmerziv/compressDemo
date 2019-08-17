7a3f5c8a06718c72fe2aafe2e4b60bdbff3ddcf7
ziv <programmerziv@com>
2019-08-17 15:51:42 +0800

    更新了一下使用map的方法

--- src/com/ziv/compress/compressDemo.java
+++ src/com/ziv/compress/compressDemo.java
@@ -6,6 +6,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.io.RandomAccessFile;
+import java.nio.MappedByteBuffer;
 import java.nio.channels.Channels;
 import java.nio.channels.FileChannel;
 import java.nio.channels.WritableByteChannel;
@@ -23,7 +25,9 @@ public class compressDemo {
 		// TODO Auto-generated method stub
 		//zipFileNoBuffer();
 		//zipFileBuffer();
-		zipFileChannel();
+		//zipFileChannel();
+		zipFileMap();
+		
 	}
 	
 	private static void printInfo(long beginTime){
@@ -83,7 +87,7 @@ public class compressDemo {
 	}
 	
 	/**
-	 * NIO的channel加持
+	 * NIO的channel加持，在内存中直接开辟了一个缓冲区，不必走内核态
 	 */
 	public static void zipFileChannel(){
 		File zipFile = new File(ZIP_File);
@@ -105,5 +109,34 @@ public class compressDemo {
 			e.printStackTrace();
 		}
 	}
-
+	/**
+	 * NIO的内存映射文件Map,直接是用户态和内核态的地址映射到一块直接物理内存上，速度贼快
+	 * 但是用直接内存有缺点啊，1.不安全2.消耗多（不是在JVM里开辟，不知道什么时候垃圾回收）3.程序无法控制（由操作系统控制住了）
+	 * 很快，和channel差不了多少
+	 */
+	public static void zipFileMap(){
+		File zipFile = new File(ZIP_File);
+		long beginTime = System.currentTimeMillis();
+		try(ZipOutputStream zipOut = new ZipOutputStream(new FileOutputStream(zipFile));
+				WritableByteChannel writableByteChannel = Channels.newChannel(zipOut)){
+			for (int i = 0; i < 10; i++) {
+				//BufferedInputStream里封装了一个byte数组用于存储数据默认一次读8192个 8M 
+				try(FileChannel  fileChannel = new  FileInputStream(JPG_File).getChannel()){
+					zipOut.putNextEntry(new ZipEntry("无所谓"+i+".jpg"));
+					// 此时用到了映射的关系
+					MappedByteBuffer mappedByteBuffer = new RandomAccessFile(JPG_File,"r")
+							.getChannel().map(FileChannel.MapMode.READ_ONLY, 0, fileChannel.size());
+					writableByteChannel.write(mappedByteBuffer);
+				}
+			}
+			// 打印耗时
+			printInfo(beginTime);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * 还有一种使用pipe的方法，是异步的，有点复杂没看懂，这里就先不写了
+	 */
 }
